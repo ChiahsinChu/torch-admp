a0449bdb1c757faef4e4019e1f5036d80a888791
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2025-12-27 13:03:39 +0800

    add auto version based on git tag

--- torch_admp/__init__.py
+++ torch_admp/__init__.py
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
+"""torch-admp: ADMP in PyTorch backend."""
 
+from ._version import __version__
 from . import base_force, electrode, nblist, optimizer, pme, qeq, recip, spatial, utils
 
 __all__ = [
+    "__version__",
     "base_force",
     "electrode",
     "nblist",
