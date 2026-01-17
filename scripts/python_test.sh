763dc60e5dfd62318fd0d608de501ff4ce4a718e
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2026-01-17 19:20:39 +0800

    add scripts used for local development.

---
+++ scripts/python_test.sh
@@ -0,0 +1,11 @@
+set -e
+
+# use pushd and popd to return to the original directory
+pushd "$(dirname "$0")/.."
+pytest --cov=torch_admp tests --cov-report=term-missing
+docstr-coverage src/torch_admp/ \
+    --skip-private \
+    --skip-property \
+    --accept-empty \
+    --exclude=".*/_version.py"
+popd
